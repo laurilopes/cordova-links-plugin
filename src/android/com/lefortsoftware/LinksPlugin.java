3d89d023b27b815c26dc383830cebba4a7c35b46
Steeve Lefort <steeve@lefort-software.com>
2014-05-25 16:39:26 +0200

    Update LinksPlugin.java

--- src/android/com/lefortsoftware/LinksPlugin.java
+++ src/android/com/lefortsoftware/LinksPlugin.java
@@ -5,6 +5,7 @@
         import org.json.JSONArray;
         import org.json.JSONException;
         import android.content.Intent;
+        import android.net.Uri;
 
         public class LinksPlugin extends CordovaPlugin{
 
